3fa9aaedf205c4cfff7398dca57347cbfe20163d
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2021-12-19 20:41:55 -0700

    created CRUD for product-routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -16,13 +16,34 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id,{
+      include: [{ model: Category, through: Tag, as: 'product_tag'}]
+    });
+
+    if(!productData) {
+      res.status(404).json({ message: 'No product found with this id!'});
+      return;
+    }
+
+    res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-});
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const productData = await Product.create(req.body);
+    res.status(200).json(productData);
+  } catch (err){
+    res.status(400).json(err)
+  }
+  });
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -51,7 +72,7 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-});
+
 
 // update product
 router.put('/:id', (req, res) => {
